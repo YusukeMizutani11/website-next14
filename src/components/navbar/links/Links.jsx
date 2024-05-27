edf4259368c4ec85ce67c51fe7e830ee5eca8ff9
Yusuke Mizutani <yusuke3rdbee@gmail.com>
2024-05-27 23:55:30 +0900

    implement logout & admin

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -23,11 +23,23 @@ const Links = () => {
         },
     ];     
 
+    // temporary
+    const session = true
+    const isAdmin = true
+
     return (
         <div className={styles.links}>
             {links.map(((link) => (
                 <NavLink item={link} key={link.title}/>
             )))}
+            {session ? (
+                <>
+                {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}} />}
+                <button className={styles.logout}>Logout</button>
+                </>
+            ) : (
+            <NavLink item={{title: "Login", path: "/login"}} />
+            )} 
         </div>
     )
 }
