eea3e27f7917f115d912ff0c747d7a2ef7bfa82d
Yusuke Mizutani <yusuke3rdbee@gmail.com>
2024-06-08 13:30:51 +0900

    single blog page design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,46 @@
+import styles from './singlePost.module.css';
+import Image from 'next/image';
+
 const SinglePostPage = () => {
     return (
-        <div>SinglePostPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="https://images.pexels.com/photos/14105926/pexels-photo-14105926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                alt="" 
+                fill 
+                className={styles.img} 
+                />
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.detail}>
+                    <Image src="https://images.pexels.com/photos/14105926/pexels-photo-14105926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                    alt="" 
+                    width={50}
+                    height={50}
+                    className={styles.avator}
+                    />
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Author</span>
+                        <span className={styles.detailValue}>Yusuke Mizutani</span>
+                    </div>
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Published</span>
+                        <span className={styles.detailValue}>01.01.2024</span>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et libero nec mi ultrices 
+                        posuere. Nullam sit amet mauris non eros consectetur tincidunt. Nulla facilisi. 
+                        Nullam nec nunc quis mi ultrices tincidunt. Integer at odio nec nunc tincidunt 
+                        ultrices. Donec nec libero ut ipsum fermentum tincidunt. Nulla facilisi. 
+                        Nullam nec nunc quis mi ultrices tincidunt. Integer at odio nec nunc tincidunt 
+                        ultrices. Donec nec libero ut ipsum fermentum tincidunt.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,13 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>aiueo</div>
+            <div className={styles.text}>
+                aiueo creative thoughts agency © All rights reserved.
+            </div>
+        </div>
     )
 }
 
