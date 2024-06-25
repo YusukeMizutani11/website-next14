efc145d03aef10ff7612e9130cc146cd19211374
Yusuke Mizutani <yusuke3rdbee@gmail.com>
2024-06-25 23:49:10 +0900

    data fetching

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,20 @@
 import Image from 'next/image';
 import styles from './singlePost.module.css';
 
-const SinglePostPage = ({params}) => {
+const getData = async (slug) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+    if(!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
 
-    console.log(params);
+    return res.json();
+}
+
+const SinglePostPage = async ({params}) => {
+
+    const {slug} = params;
+
+    const post = await getData(slug);
     return (
         <div className={styles.container}>
             <div className={styles.imgContainer}>
@@ -14,7 +25,7 @@ const SinglePostPage = ({params}) => {
                 />
             </div>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>Title</h1>
+                <h1 className={styles.title}>{post.title}</h1>
                 <div className={styles.detail}>
                     <Image src="https://images.pexels.com/photos/14105926/pexels-photo-14105926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
                     alt="" 
@@ -32,14 +43,7 @@ const SinglePostPage = ({params}) => {
                     </div>
                 </div>
                 <div className={styles.content}>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et libero nec mi ultrices 
-                        posuere. Nullam sit amet mauris non eros consectetur tincidunt. Nulla facilisi. 
-                        Nullam nec nunc quis mi ultrices tincidunt. Integer at odio nec nunc tincidunt 
-                        ultrices. Donec nec libero ut ipsum fermentum tincidunt. Nulla facilisi. 
-                        Nullam nec nunc quis mi ultrices tincidunt. Integer at odio nec nunc tincidunt 
-                        ultrices. Donec nec libero ut ipsum fermentum tincidunt.
-                    </p>
+                    {post.body}
                 </div>
             </div>
         </div>
